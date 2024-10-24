c48900074a2d07e47a3403c64c303b5f152d7577
CaoKao <bksaechao@gmail.com>
2024-10-24 03:53:39 -0700

    restructured code js into Object model

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,27 @@
 var wgGame = {
-    words: ['red', 'blue', 'green', 'yellow']
+    words: ['red', 'blue', 'green', 'yellow'],
+
+    corrGuessArr: [],
+    // Randomly picks word from words array and displays letters as underscores on html.
+    secretWord: function () {
+        var word = wgGame.words[Math.floor(Math.random() * wgGame.words.length)];
+        console.log(word);
+
+        var hiddenWord = word.split('').map(
+            function (letter) {
+                if (letter === " ") {
+                    return "\xa0"
+                } else {
+                    return " _ "
+                }
+            }
+        ).join("");
+        console.log(hiddenWord);
+        document.getElementById('test').innerHTML = hiddenWord
+    }
 }
 
-// randomly pick word
-var word = wgGame.words[Math.floor(Math.random() * wgGame.words.length)];
-console.log(word);
-// holds guesses
-var corrGuessArr = []
-
-// display word as underscores
-var hiddenWord = word.split('').map(
-    function (letter) {
-        if (letter === " ") {
-            return "\xa0"
-        } else {
-            return " _ "
-        }
-    }
-).join("");
-console.log(hiddenWord);
-document.getElementById('test').innerHTML = hiddenWord;
-
+wgGame.secretWord();
 
 document.onkeyup = (e) => {
     key = e.key 
